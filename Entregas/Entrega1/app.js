f87000ced32d4deca834ddeeb25d305432609805
KevinLi <unusualCosmonaut@gmail.com>
2026-09-23 11:06:12 +0200

    Oraculo te da pistas y mensaje de acierto

--- Entregas/Entrega1/app.js
+++ Entregas/Entrega1/app.js
@@ -7,5 +7,17 @@ const respuesta = document.querySelector("#respuesta");
 
 probar.addEventListener("click", () => {
 	const numeroRecibido = Number(intento.value);
-	respuesta.textContent = `Has dicho: ${numeroRecibido}`;
+
+	if (intento.value === "" || Number.isNaN(numeroRecibido) || numeroRecibido < 1 || numeroRecibido > 100) {
+		respuesta.textContent = "Eso no es un número válido";
+		return;
+	}
+
+	if (numeroRecibido === secreto) {
+		respuesta.textContent = "¡Correcto!";
+	} else if (numeroRecibido < secreto) {
+		respuesta.textContent = "Mi número es mayor";
+	} else {
+		respuesta.textContent = "Mi número es menor";
+	}
 });
